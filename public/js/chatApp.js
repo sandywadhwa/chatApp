e7e77c50ceb005485050a4cb698abe4daf42466d
Sandeep <sandeepkumar@sandpkr-wsm1.internal.salesforce.com>
2019-01-02 16:13:10 +0530

    On Connection Message Exchange

---
+++ public/js/chatApp.js
@@ -0,0 +1,9 @@
+var socket = io('http://localhost:3000');
+
+socket.on('message-from-server', function(evt){
+    console.log('server sent '+ evt.greeting);
+
+    socket.emit('message-from-client', {
+        greeting: 'Hello Server! I am New Client'
+    })
+})
--- server.js
+++ server.js
@@ -10,6 +10,20 @@ app.get('/', function(req, res){
   res.sendFile('index.html');
 });
 
+io.on('connection', function(socket){
+    console.log("New Client Connected");
+
+    socket.emit('message-from-server', {
+        greeting : 'Hello Client! Welcome'
+    });
+
+    socket.on('message-from-client', function(client_message){
+        console.log(client_message);
+    });
+})
+
+
+
 httpServer.listen(port, function(){
   console.log('listening on *: '+port);
 });
